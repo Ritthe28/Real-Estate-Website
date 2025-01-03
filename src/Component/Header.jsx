884cc66021fcaf036f24c34e721f1d14db0403ea
Rohit Ritthe <ritther90@gmail.com>
2025-01-03 14:21:52 +0530

    restarting

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Navbar from './navbar'
+import { assets } from '../assets/assets'
 const Header = () => {
   return (
     <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' style={{
-        backgroundImage:"url('/header_img.png')" 
+        backgroundImage: `url(${assets.header_img})`
     }} id='Header'>
       <Navbar/>
       <div className='container text-white text-center mx-auto py-4 px-6 md:px-20 lg:px-32'>
